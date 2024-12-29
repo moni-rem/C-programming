bae54d182659cf46b104f9c3352f62cec3f35e07
Mony <mony@Monys-MacBook-Air.local>
2024-12-28 22:16:09 -0800

    multi.cpp

---
+++ multi.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int duck=2, pig=3, fish=8;
+    string duckName="duck", pigName="pig", fishName="fish", amongofduck="Among of";
+
+
+    cout<<amongofduck<< duckName <<duck<<endl;
+    cout<<amongofduck<< pigName << pig<<endl;
+    cout<<amongofduck<< fishName << fish;
+    return 0;
+}
