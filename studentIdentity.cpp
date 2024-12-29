18a0730db1519b6d6b7134959b9103306dc19a6e
Mony <mony@Monys-MacBook-Air.local>
2024-12-28 23:27:48 -0800

    add studentidentity

---
+++ studentIdentity.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    string studentID="PP12213";
+    string studentname="23";
+    int studentage=75;
+    char studentgrade='B';
+    string studentgender="male";
+    int studentfee=12345;
+
+    cout<<"student ID"<<studentID<<endl;
+    cout<<"student name"<<studentname<<endl;
+    cout<<"student age"<<studentage<<endl;
+    cout<<"student grade"<<studentgrade<<endl;
+    cout<<"student gender"<<studentgender<<endl;
+    cout<<"student fee"<<studentfee<<endl;
+
+    string ID;
+    string name;
+    int age;
+    char grade;
+    string gender;
+    int fee;
+
+    cout<<"enter ID"<<endl;
+    cin>>ID;
+    cout<<"enter name"<<endl;
+    cin>>name;
+    cout<<"enter age"<<endl;
+    cin>>age;
+    cout<<"enter grade"<<endl;
+    cin>>grade;
+    cout<<"enter gender"<<endl;
+    cin>>gender;
+    cout<<"enter fee"<<endl;
+    cin>>fee;
+
+}
